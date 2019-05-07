9f73eddd76834671eef4ddb3b7e63dedc0054ac7
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-05-07 23:34:50 +0200

    Updates

---
+++ precache-manifest.5d5a3bab423b5cf629d353b9ed8d8c1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1dd3019106ba5a3822458640b01a42",
+    "url": "/padachone/index.html"
+  },
+  {
+    "revision": "ddde531a06c0c3043079",
+    "url": "/padachone/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "fd90781cd9d664215c90",
+    "url": "/padachone/static/js/2.6b30c984.chunk.js"
+  },
+  {
+    "revision": "ddde531a06c0c3043079",
+    "url": "/padachone/static/js/main.5cc70488.chunk.js"
+  },
+  {
+    "revision": "4629501c6c658ccf3226",
+    "url": "/padachone/static/js/runtime~main.a34078d7.js"
+  }
+]);
