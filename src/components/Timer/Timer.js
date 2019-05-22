f132550d0bdae3cd8793363ba6c3266ae4ca2538
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-05-22 23:23:15 +0200

    fix(time-left):add design

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,6 +2,10 @@ import React, { Component, PropTypes, useEffect, useState } from 'react'
 import Countdown from 'react-count-down'
 import Clock from 'react-live-clock';
 import {getPDdata} from '../../utils'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import './timer.css';
 
 const cb = () => {
     console.log('expired callback')
@@ -12,12 +16,29 @@ const cb = () => {
 //     cb
 //   }
 
+
+const action = (
+  <Button color="secondary" size="small">
+    Dismiss
+  </Button>
+);
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      maxWidth: 600,
+    },
+    snackbar: {
+      margin: theme.spacing(1),
+    },
+  }));
+
 const Timer = (props) => {
     const [opts, setOpts] = useState({
         // endDate: '05/21/2019 18:42',
         // prefix: 'Left for Fajr',
         // cb
     })
+    
     useEffect(() => {
         // debugger;
 
@@ -26,8 +47,16 @@ const Timer = (props) => {
         const currTime = timeopt.split(' ')[1];
         const upcomingPs = Object.entries(props.prayers).reduce((all, item) => {
             if (parseInt(item[1].split(':')[0]) >= parseInt(currTime.split(':')[0])) { //hours checking
-                all.push(item);
-                }  
+                if (parseInt(item[1].split(':')[0]) === parseInt(currTime.split(':')[0])) {
+                    if (parseInt(item[1].split(':')[1]) < parseInt(currTime.split(':')[1])) {
+                        all.push(item);
+                    }
+                }
+                else {
+                    all.push(item);
+                }
+                
+            }  
             return all
         }, [])
         if (upcomingPs.length) {
@@ -40,8 +69,25 @@ const Timer = (props) => {
         }              
         
     }, [])
+
+    const classes = useStyles();
+    const handleClose = () => {
+        debugger;
+    }
     return (
         <div className="timerComp">
+            <SnackbarContent
+                align="left"
+                className={classes.snackbar}
+                message={<Countdown 
+                    options={opts} 
+                    />}
+                onClose={handleClose}
+                action={<Button color="secondary" size="small" onClick={props.onClose}>
+                Dismiss
+                </Button>}
+                style={{backgroundColor: '#1976d2'}}
+            />
             {opts.hasOwnProperty('endDate') && <Countdown options={opts} />}
             <Clock
             // date={getPDdata('iso')}
