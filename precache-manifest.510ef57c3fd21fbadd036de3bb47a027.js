8f64f33a1bd9a8494fb807b417b4c510191a43b8
Abid Shamim PM <abidwsi@yahoo.com>
2019-05-27 21:13:01 +0200

    Updates

---
+++ precache-manifest.510ef57c3fd21fbadd036de3bb47a027.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fbd7237dbb4a3c4bb47d3f7f9c5684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d25734c59590d1fe7941",
+    "url": "/static/css/2.711045af.chunk.css"
+  },
+  {
+    "revision": "b8c6d01a9e5e6c392384",
+    "url": "/static/css/main.bb690701.chunk.css"
+  },
+  {
+    "revision": "d25734c59590d1fe7941",
+    "url": "/static/js/2.88e8b451.chunk.js"
+  },
+  {
+    "revision": "b8c6d01a9e5e6c392384",
+    "url": "/static/js/main.ce43a66d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dd3c65418ff56e0c4c4b0fd6f7b24e1a",
+    "url": "/static/media/Prayer-time.dd3c6541.jpg"
+  },
+  {
+    "revision": "d6d84b1951d54e118344294b83805881",
+    "url": "/static/media/bg-new.d6d84b19.png"
+  },
+  {
+    "revision": "9cee89c27e57f0d06ca7636242abebf0",
+    "url": "/static/media/bgmusic.9cee89c2.mp3"
+  },
+  {
+    "revision": "0b13e3dc6652d1471ee99ec56c4a5d67",
+    "url": "/static/media/boo.0b13e3dc.svg"
+  },
+  {
+    "revision": "c1619cce189e6edecd5eebd6cecd3461",
+    "url": "/static/media/cool.c1619cce.svg"
+  },
+  {
+    "revision": "599f38ab6e02ba5ef809c750b1fc9c22",
+    "url": "/static/media/goofy.599f38ab.svg"
+  },
+  {
+    "revision": "8be94c129f8e59d1ecfce1ff36c2e014",
+    "url": "/static/media/logo-sec.8be94c12.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73aca7177e25c0d038dd02bed450dbce.js"
+  "/precache-manifest.510ef57c3fd21fbadd036de3bb47a027.js"
 );
 
 self.addEventListener('message', (event) => {
