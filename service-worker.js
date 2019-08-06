ab91c612dd02e9445f1bb42525f2a9e5f04b36f6
Abid Shamim P.M <abid.shamim.p.m@ing.com>
2019-08-06 10:44:43 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79b568a551975181a9b554cbff92a26d.js"
+  "/precache-manifest.2b3458d58bc85470c74fc296db53d481.js"
 );
 
 self.addEventListener('message', (event) => {
